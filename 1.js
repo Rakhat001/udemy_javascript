f6db532dfde59cde8b888d68240db74b27f52377
Rakhat001 <rakhat.ismailov.2001@gmail.com>
2023-03-16 02:01:26 +0300

    3 lab

--- 1.js
+++ 1.js
@@ -1,6 +1,19 @@
 
 'use strict';
-const numberOfFilms = prompt('Сколько фильмоы вы посмотрели?', '');
+
+let numberOfFilms;
+
+function start(){
+  numberOfFilms = +prompt('Сколько фильмоы вы посмотрели?', '');
+
+  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = +prompt('Сколько фильмоы вы посмотрели?', '');
+
+  }
+}
+start();
+
+
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -9,17 +22,23 @@ const personalMovieDB = {
   privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько вы  оцените?', '');
+function rememberMyFilms(){
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько вы  оцените?', '');
+  
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+    }
+    else {
+      i--;
+    }
+}
+}
 
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-  }
-  else {
-    i--;
-  }
+rememberMyFilms();
 
+function detectPersonalLevel(){
   if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
   } else if (personalMovieDB.count > 10 && personalMovieDB > 30) {
@@ -29,6 +48,23 @@ for (let i = 0; i < 2; i++) {
   } else {
     console.log("Произошла ошибка")
   }
-
 }
-console.log(personalMovieDB);
+
+detectPersonalLevel();
+
+
+function showMyDatabase(hidden){
+  if (!hidden){
+    console.log(personalMovieDB)
+  }
+}
+showMyDatabase(personalMovieDB.privat);
+
+function writeYpurGenres(){
+  for(let i=1; i<=3; i++){
+    personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+writeYpurGenres()
+
+
