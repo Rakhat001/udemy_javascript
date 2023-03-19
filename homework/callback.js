3a31ab94db34de5239788cdb62dda72a44c84585
Rakhat001 <rakhat.ismailov.2001@gmail.com>
2023-03-20 00:33:09 +0300

    zxc

---
+++ homework/callback.js
@@ -0,0 +1,21 @@
+function first() {
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJs(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+learnJs('Javascript', function () {
+    console.log('я прошел этот урок');
+});
