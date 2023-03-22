4db6969e07674a2bb77543bc0f487e6984da1fd8
Rakhat001 <rakhat.ismailov.2001@gmail.com>
2023-03-22 03:38:04 +0300

    array

---
+++ homework/Array_and_.js
@@ -0,0 +1,18 @@
+const arr =[1,2,3,4,6,8];
+arr.pop();
+arr.push(10);
+
+arr.forEach(function(item,i,arr){
+    console.log(`${i}: ${item} vnutri massiva ${arr}`);
+}) 
+
+for(let value of arr){
+    console.log(value);
+}
+ const str = prompt("", "");
+ const products = str.split(",");
+ console.log(products);
+ console.log(products.join)
+
+
+
